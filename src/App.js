8c59325b97bb99f2fbae12dd150aeb2bb2f1ec67
pmAdriaan <pmAdriaan@users.noreply.github.com>
2024-01-21 12:37:14 +0000

    Created the HERO section

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Nav from './components/Nav';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Nav />
-      <Header />
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
       <Main />
       <Footer />
     </>
